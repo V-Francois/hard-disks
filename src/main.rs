fd139866fca1982705fdcb0013113aaf4c5e21d6
François Villemot <francois.villemot@aqemia.com>
2024-07-26 21:01:20 +0200

    feat: check that the initial configuration has no overlaps

--- src/main.rs
+++ src/main.rs
@@ -17,4 +17,17 @@ fn main() {
             disks.push(new_disk);
         }
     }
+
+    let sim_box = geometry::Box { lx: 20.0, ly: 20.0 };
+    // check that the starting conf has no overlaps
+    for i in 0..disks.len() {
+        for j in 0..disks.len() {
+            if i == j {
+                continue;
+            }
+            assert!(!disks::are_disks_overlapping(
+                &disks[i], &disks[j], &sim_box
+            ));
+        }
+    }
 }
