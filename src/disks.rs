7618eb9f2f1236b905eb934e0ed3b34ec15a59bc
François Villemot <francois.villemot@aqemia.com>
2024-07-26 20:37:35 +0200

    feat: implement periodic boundary conditions

--- src/disks.rs
+++ src/disks.rs
@@ -1,4 +1,5 @@
-use crate::geometry::distance_sq;
+use crate::geometry::distance_sq_periodic;
+use crate::geometry::Box;
 use crate::geometry::Position;
 
 pub struct Disk {
@@ -6,8 +7,8 @@ pub struct Disk {
     pub radius: f64,
 }
 
-pub fn are_disks_overlapping(disk_1: &Disk, disk_2: &Disk) -> bool {
-    let rsq = distance_sq(&disk_1.position, &disk_2.position);
+pub fn are_disks_overlapping(disk_1: &Disk, disk_2: &Disk, sim_box: &Box) -> bool {
+    let rsq = distance_sq_periodic(&disk_1.position, &disk_2.position, &sim_box);
     let sigma_sq = (disk_1.radius + disk_2.radius) * (disk_1.radius + disk_2.radius);
     return rsq < sigma_sq;
 }
@@ -19,6 +20,9 @@ mod tests {
 
     #[test]
     fn test_overalap() {
+        let sim_box_1 = Box { lx: 1.0, ly: 10.0 };
+        let sim_box_2 = Box { lx: 1.0, ly: 6.0 };
+
         let disk_1 = Disk {
             position: Position { x: 0.0, y: 0.0 },
             radius: 2.0,
@@ -33,7 +37,8 @@ mod tests {
             position: Position { x: 0.0, y: 5.0 },
             radius: 2.0,
         };
-        assert!(are_disks_overlapping(&disk_1, &disk_2));
-        assert!(!are_disks_overlapping(&disk_1, &disk_3));
+        assert!(are_disks_overlapping(&disk_1, &disk_2, &sim_box_1));
+        assert!(!are_disks_overlapping(&disk_1, &disk_3, &sim_box_1));
+        assert!(are_disks_overlapping(&disk_1, &disk_3, &sim_box_2));
     }
 }
--- src/geometry.rs
+++ src/geometry.rs
@@ -3,9 +3,30 @@ pub struct Position {
     pub y: f64,
 }
 
+pub struct Box {
+    pub lx: f64,
+    pub ly: f64,
+}
+
 pub fn distance_sq(pos_1: &Position, pos_2: &Position) -> f64 {
     let dx = pos_1.x - pos_2.x;
     let dy = pos_1.y - pos_2.y;
+    return dx * dx + dy * dy;
+}
+
+pub fn distance_sq_periodic(pos_1: &Position, pos_2: &Position, sim_box: &Box) -> f64 {
+    let mut dx = pos_1.x - pos_2.x;
+    if dx > sim_box.lx / 2.0 {
+        dx -= sim_box.lx;
+    } else if dx < -sim_box.lx / 2.0 {
+        dx += sim_box.lx;
+    }
+    let mut dy = pos_1.y - pos_2.y;
+    if dy > sim_box.ly / 2.0 {
+        dy -= sim_box.ly;
+    } else if dy < -sim_box.ly / 2.0 {
+        dy += sim_box.ly;
+    }
     return dx * dx + dy * dy;
 }
 
@@ -18,5 +39,13 @@ mod tests {
         let pos_1 = Position { x: 0.0, y: 0.0 };
         let pos_2 = Position { x: 1.0, y: 2.0 };
         assert_eq!(distance_sq(&pos_1, &pos_2), 5.0);
+    }
+
+    #[test]
+    fn test_distance_periodic() {
+        let pos_1 = Position { x: 0.0, y: 0.0 };
+        let pos_2 = Position { x: 0.0, y: 4.0 };
+        let sim_box = Box { lx: 1.0, ly: 5.0 };
+        assert_eq!(distance_sq_periodic(&pos_1, &pos_2, &sim_box), 1.0);
     }
 }
