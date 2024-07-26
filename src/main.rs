e9fe872608273907fe4bfd0623269a560fba8ee2
François Villemot <francois.villemot@aqemia.com>
2024-07-26 20:50:47 +0200

    feat: initialize the sim

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,20 @@
 pub mod disks;
 pub mod geometry;
 
-fn main() {}
+fn main() {
+    let radius = 0.5;
+    let mut disks: Vec<disks::Disk> = Vec::new();
+    for i in 0..10 {
+        for j in 0..10 {
+            let pos = geometry::Position {
+                x: i as f64,
+                y: j as f64,
+            };
+            let new_disk = disks::Disk {
+                position: pos,
+                radius: radius,
+            };
+            disks.push(new_disk);
+        }
+    }
+}
