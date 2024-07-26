18d3576d87061634f94452b2d62785a56c46b6a7
François Villemot <francois.villemot@aqemia.com>
2024-07-26 20:24:46 +0200

    feat: use position struct in disks struct

--- src/disks.rs
+++ src/disks.rs
@@ -1,13 +1,13 @@
+use crate::geometry::distance_sq;
+use crate::geometry::Position;
+
 pub struct Disk {
-    pub x: f64,
-    pub y: f64,
+    pub position: Position,
     pub radius: f64,
 }
 
 pub fn are_disks_overlapping(disk_1: &Disk, disk_2: &Disk) -> bool {
-    let dx = disk_1.x - disk_2.x;
-    let dy = disk_1.y - disk_2.y;
-    let rsq = dx * dx + dy * dy;
+    let rsq = distance_sq(&disk_1.position, &disk_2.position);
     let sigma_sq = (disk_1.radius + disk_2.radius) * (disk_1.radius + disk_2.radius);
     return rsq < sigma_sq;
 }
@@ -20,20 +20,17 @@ mod tests {
     #[test]
     fn test_overalap() {
         let disk_1 = Disk {
-            x: 0.0,
-            y: 0.0,
+            position: Position { x: 0.0, y: 0.0 },
             radius: 2.0,
         };
 
         let disk_2 = Disk {
-            x: 0.0,
-            y: 3.0,
+            position: Position { x: 0.0, y: 3.0 },
             radius: 2.0,
         };
 
         let disk_3 = Disk {
-            x: 0.0,
-            y: 5.0,
+            position: Position { x: 0.0, y: 5.0 },
             radius: 2.0,
         };
         assert!(are_disks_overlapping(&disk_1, &disk_2));
