d8ee1aa675a4e94a9c9ba9ac74c380dae8c6431b
François Villemot <francois.villemot@aqemia.com>
2024-07-27 22:13:49 +0200

    feat(plot-conf): get command line arguments

--- src/bin/plot-conf.rs
+++ src/bin/plot-conf.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello world");
+use std::env;
+use std::process;
+
+fn main() -> process::ExitCode {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        eprintln!("Usage: {} path-to-position-file", args[0]);
+        return process::ExitCode::from(1);
+    }
+
+    let file_path = &args[1];
+    println!("filepath: {}", file_path);
+
+    process::ExitCode::SUCCESS
 }
