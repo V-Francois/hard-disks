d711cb5513aabb0c5b3861ce1f35eb93f5f9b847
François Villemot <francois.villemot@aqemia.com>
2024-07-29 14:41:30 +0200

    refactor: move nvt sampling to a sub-function

--- src/hard-disks.rs
+++ src/hard-disks.rs
@@ -1,10 +1,9 @@
-use rand;
-use rand::Rng;
 use std::path;
 
 pub mod disks;
 pub mod geometry;
 pub mod io;
+pub mod sample;
 
 fn main() {
     let radius = 0.5;
@@ -31,40 +30,8 @@ fn main() {
     io::write_coords_to_file(&disks, &sim_box, filepath);
 
     let nb_steps = 10000;
-    let max_displacement = 0.5;
-    let mut rng = rand::thread_rng();
-    let mut nb_success = 0;
-    let nb_disks = disks.len() as u32;
-    for _ in 0..nb_steps {
-        let theta: f64 = rng.gen();
-        let r: f64 = rng.gen();
-        let dx = r * theta.cos() * max_displacement;
-        let dy = r * theta.sin() * max_displacement;
-
-        let disk_index: usize = rng.gen_range(0..nb_disks).try_into().unwrap();
-
-        disks[disk_index].position.x += dx;
-        disks[disk_index].position.y += dy;
-
-        let mut overlaps = false;
-        for j in 0..disks.len() {
-            if j == disk_index {
-                continue;
-            }
-            if disks::are_disks_overlapping(&disks[disk_index], &disks[j], &sim_box) {
-                overlaps = true;
-                break;
-            }
-        }
-
-        if overlaps {
-            disks[disk_index].position.x -= dx;
-            disks[disk_index].position.y -= dy;
-        } else {
-            nb_success += 1;
-        }
-    }
-    println!("Success rate: {}", (nb_success as f32) / (nb_steps as f32));
+    let acceptance_rate = sample::sample_nvt(&mut disks, &sim_box, nb_steps);
+    println!("Acceptance rate: {}", acceptance_rate);
 
     let filepath = path::Path::new("final.txt");
     io::write_coords_to_file(&disks, &sim_box, filepath);
