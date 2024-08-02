e3aed62270d07cfedf7404507aabd6b39d8b7244
François Villemot <francois.villemot@aqemia.com>
2024-08-02 22:34:33 +0200

    feat: read config from yaml file

--- src/hard-disks.rs
+++ src/hard-disks.rs
@@ -1,5 +1,7 @@
+use std::env;
 use std::path;
 
+pub mod config;
 pub mod disks;
 pub mod geometry;
 pub mod io;
@@ -7,16 +9,29 @@ pub mod sample;
 pub mod state;
 
 fn main() {
-    let mut state = state::State::hexagonal_packing(10, 10, 0.8);
+    let args: Vec<String> = env::args().collect();
 
-    let filepath = path::Path::new("initial.txt");
-    io::write_coords_to_file(&state, filepath);
+    if args.len() != 2 {
+        panic!("Usage: {} config.yaml", args[0]);
+    }
 
-    let nb_steps = 1e7 as u32;
-    let acceptance_rate = sample::sample_npt(&mut state, 10.0, nb_steps);
-    //let acceptance_rate = sample::sample_nvt(&mut state, nb_steps);
-    println!("Acceptance rate: {}", acceptance_rate);
+    let file_path = &args[1];
+    if !path::Path::new(file_path).exists() {
+        panic!("Path {} doesn’t exist", file_path);
+    }
 
-    let filepath = path::Path::new("final.txt");
-    io::write_coords_to_file(&state, filepath);
+    let config = config::Config::from_yaml_file(file_path.as_str());
+
+    //let mut state = state::State::hexagonal_packing(10, 10, 0.8);
+    //
+    //let filepath = path::Path::new("initial.txt");
+    //io::write_coords_to_file(&state, filepath);
+    //
+    //let nb_steps = 1e7 as u32;
+    //let acceptance_rate = sample::sample_npt(&mut state, 10.0, nb_steps);
+    ////let acceptance_rate = sample::sample_nvt(&mut state, nb_steps);
+    //println!("Acceptance rate: {}", acceptance_rate);
+    //
+    //let filepath = path::Path::new("final.txt");
+    //io::write_coords_to_file(&state, filepath);
 }
