64f43c5c08b97db3325629f9a9e5e531d09006b3
François Villemot <56830249+V-Francois@users.noreply.github.com>
2024-08-09 13:56:11 +0200

    feat: benchmark to compare results from NPT and NVT (#7)
    There are discrepancies.

--- benchmark/analyze_benchmark.py
+++ benchmark/analyze_benchmark.py
@@ -3,29 +3,63 @@ import pandas as pd
 import glob
 import seaborn as sns
 import matplotlib.pyplot as plt
-import os
-import time
 import pymser
+import yaml
 
 
 DISK_DIAMETER = a0 = 1
 DISK_VOLUME = (DISK_DIAMETER / 2) ** 2 * np.pi
 
 
+def pressure_from_yaml(file_path: str, plot=False) -> dict:
+    packing_fraction = float(file_path.split("_")[2].strip(".yaml"))
+    disk_surface_area = np.pi * (0.5) ** 2
+    number_density = packing_fraction / disk_surface_area
+
+    with open(file_path) as f:
+        results = yaml.safe_load(f)
+
+    df = pd.DataFrame(
+        {"g": results["g_of_r"]["normalized_g"], "r": results["g_of_r"]["r"]}
+    )
+
+    coefs = np.polyfit(df["r"], df["g"], deg=4)
+    xs = np.linspace(min(df["r"]), max(df["r"]))
+    ys = (
+        coefs[0] * xs**4
+        + coefs[1] * xs**3
+        + coefs[2] * xs**2
+        + coefs[3] * xs
+        + coefs[4]
+    )
+
+    value_at_0 = np.sum(coefs)
+
+    p_over_kt = number_density * (1 + 2 * packing_fraction * value_at_0)
+
+    if plot:
+        sns.scatterplot(data=df, x="r", y="g")
+        plt.plot(xs, ys)
+        plt.show()
+
+    return {"pressure": p_over_kt, "density": packing_fraction}
+
+
 def average_volume_from_packing_fraction(packing_fraction: float) -> float:
     return DISK_VOLUME / packing_fraction
 
 
-def data_from_result_file(file_path: str, plot=False) -> dict:
-    pressure = float(os.path.basename(file_path).split("_")[-1].strip(".csv"))
+def density_for_yaml(file_path: str, plot=False) -> dict:
+    pressure = float(file_path.split("_")[2].strip(".yaml"))
 
-    df = pd.read_csv(file_path)
-    df["volume"] = df["density"].apply(average_volume_from_packing_fraction)
+    with open(file_path) as f:
+        results = yaml.safe_load(f)
+
+    df = pd.DataFrame({"density": results["density"], "step": results["step"]})
 
     # Run pyMSER to get equilibration data
-    start_time = time.time()
     results = pymser.equilibrate(
-        df["volume"],
+        df["density"],
         LLM=True,
         batch_size=1,
         ADF_test=True,
@@ -33,28 +67,24 @@ def data_from_result_file(file_path: str, plot=False) -> dict:
         print_results=False,
     )
 
-    end_time = time.time() - start_time
-    # Print the results
-    # print(f"pyMSER Execution time:               {end_time:.3f} s")
-
     if plot:
         df["step"] = [int(i) for i in df.index]
-        ax = sns.scatterplot(data=df, x="step", y="volume")
+        ax = sns.scatterplot(data=df, x="step", y="density")
         ax.plot(
-            range(len(df["volume"]))[results["t0"] :],
+            range(len(df["density"]))[results["t0"] :],
             results["equilibrated"],
             label="Equilibrated data",
             color="red",
         )
         ax.plot(
-            [0, len(df["volume"])],
+            [0, len(df["density"])],
             [results["average"], results["average"]],
             color="green",
             zorder=4,
             label="Equilibrated average",
         )
         ax.fill_between(
-            range(len(df["volume"])),
+            range(len(df["density"])),
             results["average"] - results["uncertainty"],
             results["average"] + results["uncertainty"],
             color="lightgreen",
@@ -66,39 +96,30 @@ def data_from_result_file(file_path: str, plot=False) -> dict:
 
     return {
         "pressure": pressure,
-        "volume": results["average"],
-        "volume_err": results["uncertainty"],
+        "density": results["average"],
+        "density_err": results["uncertainty"],
     }
 
 
 if __name__ == "__main__":
-    # Data from 10.1103/physreve.55.750
-    df_ref = pd.DataFrame(
-        {
-            "pressure": [9, 8.5, 8, 7.5, 7, 6.5, 6],
-            "volume": [1.23, 1.245, 1.255, 1.32, 1.35, 1.37, 1.4],
-        }
+
+    npt_files = glob.glob("./results/results_npt*")
+    df_npt = pd.DataFrame.from_records(
+        [density_for_yaml(f, plot=False) for f in npt_files]
     )
-    df_ref["origin"] = "Ref"
+    df_npt["origin"] = "NPT"
 
-    result_files = glob.glob("./results-256/*.csv")
-    df_res_256 = pd.DataFrame.from_records(
-        [data_from_result_file(f, plot=False) for f in result_files]
+    nvt_files = glob.glob("./results/results_nvt*")
+    df_nvt = pd.DataFrame.from_records(
+        [pressure_from_yaml(f, plot=False) for f in nvt_files]
     )
-    df_res_256["origin"] = "Me - 256"
+    df_nvt["origin"] = "NVT"
+    df_nvt["pressure"] = df_nvt["pressure"].apply(lambda x: x)
 
-    result_files = glob.glob("./results/*.csv")
-    df_res_100 = pd.DataFrame.from_records(
-        [data_from_result_file(f, plot=False) for f in result_files]
-    )
-    df_res_100["origin"] = "Me - 100"
-
-    # df_res["pressure_corrected"] = df_res["pressure"]
-
-    sns.scatterplot(
-        data=pd.concat([df_res_256, df_res_100, df_ref]),
-        x="volume",
+    ax = sns.scatterplot(
+        data=pd.concat([df_npt, df_nvt]),
         y="pressure",
+        x="density",
         hue="origin",
     )
     plt.show()
